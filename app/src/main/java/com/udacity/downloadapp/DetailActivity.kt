15f85462b562769374b90f151e7ebb0f4879a7fb
Mustafa Raafat <mustafa.raft@hotmail.com>
2022-12-13 10:34:17 +0200

    first commit

---
+++ app/src/main/java/com/udacity/downloadapp/DetailActivity.kt
@@ -0,0 +1,15 @@
+package com.udacity.downloadapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.udacity.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+//        setSupportActionBar(toolbar)
+    }
+
+}
